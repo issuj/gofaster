c020f320c231b8bb4d37258955cbce69beb788dc
Jussi Holm <jussi@pokko.net>
2015-05-29 19:45:21 +0300

    - using faster base64 encoder

--- base64_test.go
+++ base64_test.go
@@ -3,20 +3,26 @@ package base64
 import "encoding/base64"
 import "testing"
 
-const base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+//import "fmt"
 
 func TestBase64Raw(t *testing.T) {
 	var in, out, expected, alphabet []byte
 	in = make([]byte, 258)
 	out = make([]byte, 344)
 	expected = make([]byte, 344)
-	alphabet = []byte(base64Alphabet)
+	alphabet = []byte(base64StdAlphabet)
 	for i := 0; i < 256; i++ {
 		in[i] = uint8(i)
 	}
-	base64_enc(out, in, alphabet)
+	read, wrote := base64_enc(out, in, alphabet)
 	base64.StdEncoding.Encode(expected, in)
 	// Verify the input buffer is unchanged
+	if read != 258 {
+		t.Fatal("Wrong amount read", read)
+	}
+	if wrote != 344 {
+		t.Fatal("Wrong amount wrote", read)
+	}
 	for i, v := range in {
 		if i < 256 {
 			if v != uint8(i) {
@@ -28,7 +34,6 @@ func TestBase64Raw(t *testing.T) {
 			}
 		}
 	}
-
 	for i, v := range out {
 		if v != expected[i] {
 			t.Fatal("Wrong output at byte", i)
@@ -52,7 +57,7 @@ func BenchmarkSimd(b *testing.B) {
 	var in, out, alphabet []byte
 	in = make([]byte, bench_b64_words*3)
 	out = make([]byte, bench_b64_words*4)
-	alphabet = []byte(base64Alphabet)
+	alphabet = []byte(base64StdAlphabet)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		base64_enc(out, in, alphabet)
